38bc12da9d85daedf40d88f837e498e3ede33699
bsmith175 <benlismith@gmail.com>
2020-05-02 00:16:42 -0500

    removed unused constants

--- src/main/java/edu/brown/cs/teams/constants/Constants.java
+++ src/main/java/edu/brown/cs/teams/constants/Constants.java
@@ -8,13 +8,4 @@ public class Constants {
     public static final String EMBEDDINGS_FILE = "data/ingredient_vectors.json";
 
 
-    public static final String DB_HOST = "johnny.heliohost.org";
-    public static final String DB_NAME = "bsmith28_wiyf_db";
-    public static final String DB_PORT = "5432";
-    public static final String DB_USERNAME = "bsmith28_ben";
-    //TODO: encrypt this
-    public static final String DB_PWD = "wiyf1!";
-    public static final String GOOGLE_CLIENT_ID = "727828985870-25d5atf1g5r853t5n6o90u0dansh9ao2.apps.googleusercontent.com";
-
-
 }
